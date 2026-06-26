cb7f0fb567ad4e30090fec86c29a91f0919aabbe
Syeda Nazia <142724168+SyedaNaziaGit@users.noreply.github.com>
2026-06-26 19:41:41 +0530

    Add files via upload

---
+++ langChain_TextSplitters/lengthBased_textsplitter.py
@@ -0,0 +1,15 @@
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("Linux Interview Guide.pdf")
+docs = loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 0,
+    separator = ""
+)
+
+#result = splitter.split_text(text)
+result = splitter.split_documents(docs)
+print(result[0].page_content)#first chunk after split - this is also a document  obj having pagecontent and metadata keys in each  chunk 
